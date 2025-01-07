2fd6493967d02fde90799e4c539ccccd593595dc
Li Pian Capri <capri@Lis-iMac.local>
2025-01-07 13:57:18 +0800

    Fix module configuration for frontend

--- frontend/dist/index.js
+++ frontend/dist/index.js
@@ -1,4 +1,3 @@
-"use strict";
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -8,16 +7,12 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const axios_1 = __importDefault(require("axios"));
+import axios from 'axios';
 const backendUrl = "https://dividendchecker-backend.onrender.com";
 function fetchBackendMessage() {
     return __awaiter(this, void 0, void 0, function* () {
         try {
-            const response = yield axios_1.default.get(`${backendUrl}/`);
+            const response = yield axios.get(`${backendUrl}/`);
             console.log('Backend Response:', response.data);
         }
         catch (error) {
