dbfbfe972a150835a2c8a9c8f5e2add27fc12f2a
Li Pian Capri <capri@Lis-iMac.local>
2025-01-07 13:53:19 +0800

    Fix module settings for deployment

--- frontend/dist/index.js
+++ frontend/dist/index.js
@@ -1,3 +1,4 @@
+"use strict";
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -7,12 +8,16 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
-import axios from 'axios';
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const axios_1 = __importDefault(require("axios"));
 const backendUrl = "https://dividendchecker-backend.onrender.com";
 function fetchBackendMessage() {
     return __awaiter(this, void 0, void 0, function* () {
         try {
-            const response = yield axios.get(`${backendUrl}/`);
+            const response = yield axios_1.default.get(`${backendUrl}/`);
             console.log('Backend Response:', response.data);
         }
         catch (error) {
